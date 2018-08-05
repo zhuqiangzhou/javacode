f5452f94dcab0d0a3a66b4d0e09ad53f7e841888
zhuqiangzhou <zqzcool02452>
2018-08-05 21:58:41 +0800

    提交代码

--- spring-annotation/src/test/java/com/zqz/test/IOCTest.java
+++ spring-annotation/src/test/java/com/zqz/test/IOCTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.zqz.config.MainConfig;
+import com.zqz.config.MainConfig2;
 
 public class IOCTest {
 	
@@ -11,6 +12,15 @@ public class IOCTest {
 	@Test
 	public void test() {
 		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfig.class);
+		String[] names = applicationContext.getBeanDefinitionNames();
+		for (String name : names) {
+			System.out.println(name);
+		}
+	}
+	
+	@Test
+	public void test01() {
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfig2.class);
 		String[] names = applicationContext.getBeanDefinitionNames();
 		for (String name : names) {
 			System.out.println(name);

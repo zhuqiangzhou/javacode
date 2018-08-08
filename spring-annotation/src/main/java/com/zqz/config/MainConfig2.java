7d62ba4fd013acfdfa9ab4fef21ca946b78ea252
zhuqiangzhou <zhuqiangzhou@sinosoft.com.cn>
2018-08-08 09:54:51 +0800

    更新代码

--- spring-annotation/src/main/java/com/zqz/config/MainConfig2.java
+++ spring-annotation/src/main/java/com/zqz/config/MainConfig2.java
@@ -12,10 +12,11 @@ import com.zqz.beans.Color;
 import com.zqz.beans.Person;
 import com.zqz.beans.Red;
 import com.zqz.condition.LinuxCondition;
+import com.zqz.condition.MyImportSelector;
 import com.zqz.condition.WindowsCondition;
 @Conditional({WindowsCondition.class})
 @Configuration
-@Import({Color.class,Red.class})
+@Import({Color.class,Red.class,MyImportSelector.class,MyImportBeanDefinitionRegistrar.class})
 public class MainConfig2 {
 	
 	//默认是单实例的
@@ -64,6 +65,8 @@ public class MainConfig2 {
 	 * 1）包扫描+组件标注注解（@Controller、@Service、@Componen..）[自己写的类]
 	 * 2)@Bean[导入的第三方包里面的组件]
 	 * 3）@Import[快速给容器中导入一个组件]
-	 * 
+	 * 	@Import,容器中就会自动注册这个组件，id默认是全类名
+	 * 	@ImportSelector:返回需要导入的组件的全类名数组
+	 *  @ImportBeanDefinitionRegistrar
 	 */
 }
